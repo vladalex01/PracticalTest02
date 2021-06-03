0c10141441e0aa90d6acaea29cf50ddebed2d081
Vlad Catanoiu <44285701+vladalex01@users.noreply.github.com>
2021-06-03 12:30:16 +0300

    Add interface

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02.java
@@ -1,14 +1,85 @@
 package ro.pub.cs.systems.eim.practicaltest02;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import ro.pub.cs.systems.eim.practicaltest02.R;
+import ro.pub.cs.systems.eim.practicaltest02.general.Constants;
 
 public class PracticalTest02 extends AppCompatActivity {
+    // Server widgets
+    private EditText serverPortEditText = null;
+    private Button connectButton = null;
+
+    // Client widgets
+    private EditText clientAddressEditText = null;
+    private EditText clientPortEditText = null;
+    private Button getCurrencyButton = null;
+    private Spinner informationTypeSpinner = null;
+    private TextView currencyTextView = null;
+
+
+    private ConnectButtonClickListener connectButtonClickListener = new ConnectButtonClickListener();
+    private class ConnectButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String serverPort = serverPortEditText.getText().toString();
+            if (serverPort == null || serverPort.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Server port should be filled!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Log.d(Constants.TAG, "[MAIN ACTIVITY] Server port: " + serverPort);
+        }
+
+    }
+
+    private GetCurrencyButtonClickListener getCurrencyButtonClickListener = new GetCurrencyButtonClickListener();
+    private class GetCurrencyButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String clientAddress = clientAddressEditText.getText().toString();
+            String clientPort = clientPortEditText.getText().toString();
+            if (clientAddress == null || clientAddress.isEmpty()
+                    || clientPort == null || clientPort.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Client connection parameters should be filled!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Log.d(Constants.TAG, "[MAIN ACTIVITY] Client port: " + clientPort);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(Constants.TAG, "[MAIN ACTIVITY] onCreate() callback method has been invoked");
         setContentView(R.layout.activity_main);
+
+        serverPortEditText = (EditText)findViewById(R.id.server_port_edit_text);
+        connectButton = (Button)findViewById(R.id.connect_button);
+        connectButton.setOnClickListener(connectButtonClickListener);
+
+        clientAddressEditText = (EditText)findViewById(R.id.client_address_edit_text);
+        clientPortEditText = (EditText)findViewById(R.id.client_port_edit_text);
+        informationTypeSpinner = (Spinner)findViewById(R.id.information_type_spinner);
+        getCurrencyButton = (Button)findViewById(R.id.get_weather_forecast_button);
+        getCurrencyButton.setOnClickListener(getCurrencyButtonClickListener);
+        currencyTextView = (TextView)findViewById(R.id.currency_view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(Constants.TAG, "[MAIN ACTIVITY] onDestroy() callback method has been invoked");
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/general/Constants.java
@@ -0,0 +1,19 @@
+package ro.pub.cs.systems.eim.practicaltest02.general;
+
+public interface Constants {
+
+    final public static String TAG = "[PracticalTest02]";
+
+    final public static boolean DEBUG = true;
+
+    final public static String WEB_SERVICE_MODE = "json";
+    //final public static String WEB_SERVICE_ADDRESS = "https://api.coindesk.com/v1/bpi/currentprice";
+
+    final public static String EUR = "EUR";
+    final public static String USD = "USD";
+
+    final public static String EMPTY_STRING = "";
+    final public static String MAIN = "main";
+
+}
+
