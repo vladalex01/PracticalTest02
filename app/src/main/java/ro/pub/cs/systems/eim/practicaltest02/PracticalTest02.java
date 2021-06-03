2cebc2cb1f8ddadf80c0252bc079f4953fceef6e
Vlad Catanoiu <44285701+vladalex01@users.noreply.github.com>
2021-06-03 12:07:41 +0300

    First commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalTest02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
